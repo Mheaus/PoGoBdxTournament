89d490f0d452c9bd1fbac0d0db4b56aef7a25e96
mathieu audebert <mathieu.adbrt@gmail.com>
2019-02-17 13:04:01 +0100

    Updates

---
+++ precache-manifest.967f72abe4bab07e5c5185a8111af54e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/po-go-bdx-tournament/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "87674a31cc48c563fbbe",
+    "url": "/po-go-bdx-tournament/static/js/runtime~main.87674a31.js"
+  },
+  {
+    "revision": "845917968c45b910ca83",
+    "url": "/po-go-bdx-tournament/static/js/main.84591796.chunk.js"
+  },
+  {
+    "revision": "ba1ca5ba3f9c1e3c91ad",
+    "url": "/po-go-bdx-tournament/static/js/2.ba1ca5ba.chunk.js"
+  },
+  {
+    "revision": "845917968c45b910ca83",
+    "url": "/po-go-bdx-tournament/static/css/main.ac4cb06c.chunk.css"
+  },
+  {
+    "revision": "2f2f19448d8aa23d2d87c30744846501",
+    "url": "/po-go-bdx-tournament/index.html"
+  }
+];
